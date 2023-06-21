52220aafeba4d5759425aad8485b69a9e81d3067
Alex Petit <alexpetit6@gmail.com>
2023-06-21 11:11:55 -0700

    update total macros and calories

--- main_app/models.py
+++ main_app/models.py
@@ -20,6 +20,10 @@ class Meal(models.Model):
   breakfast = models.ManyToManyField(Food, blank=True, related_name='breakfast')
   lunch = models.ManyToManyField(Food, blank=True, related_name='lunch')
   dinner = models.ManyToManyField(Food, blank=True, related_name='dinner')
+  calories = models.IntegerField(default=0)
+  carbs = models.IntegerField(default=0)
+  protein = models.IntegerField(default=0)
+  fat = models.IntegerField(default=0)
   date = models.DateField()
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   def __str__(self):
@@ -28,4 +32,4 @@ class Meal(models.Model):
 class MealFood(models.Model):
   meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
   food = models.ForeignKey(Food, on_delete=models.CASCADE)
-  quantity = models.PositiveIntegerField(default=100)
+  quantity = models.PositiveIntegerField(default=100) 
